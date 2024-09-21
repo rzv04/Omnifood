f55b1b93f58dfa14f111c6f1f11014a61f58ff29
YoloTheBEst <mrs_razvan2004@yahoo.com>
2024-09-21 15:54:30 +0300

    Add FAQ section

--- js/script.js
+++ js/script.js
@@ -70,55 +70,16 @@ function checkFlexGap() {
 }
 checkFlexGap();
 
-// https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
-
-/*
-.no-flexbox-gap .main-nav-list li:not(:last-child) {
-  margin-right: 4.8rem;
-}
-
-.no-flexbox-gap .list-item:not(:last-child) {
-  margin-bottom: 1.6rem;
-}
-
-.no-flexbox-gap .list-icon:not(:last-child) {
-  margin-right: 1.6rem;
-}
-
-.no-flexbox-gap .delivered-faces {
-  margin-right: 1.6rem;
-}
-
-.no-flexbox-gap .meal-attribute:not(:last-child) {
-  margin-bottom: 2rem;
-}
-
-.no-flexbox-gap .meal-icon {
-  margin-right: 1.6rem;
-}
-
-.no-flexbox-gap .footer-row div:not(:last-child) {
-  margin-right: 6.4rem;
-}
-
-.no-flexbox-gap .social-links li:not(:last-child) {
-  margin-right: 2.4rem;
-}
-
-.no-flexbox-gap .footer-nav li:not(:last-child) {
-  margin-bottom: 2.4rem;
-}
-
-@media (max-width: 75em) {
-  .no-flexbox-gap .main-nav-list li:not(:last-child) {
-    margin-right: 3.2rem;
+// Toggle FAQ section answers
+const questionsList = document.querySelector(".questions-list");
+questionsList.addEventListener("click", function (e) {
+  const clickParentTarget = e.target.parentNode;
+  if (clickParentTarget.matches(".faq-button")) {
+    const parentQuestion = clickParentTarget.parentNode.parentNode;
+    const questionParagraph = parentQuestion.querySelector("p");
+    const [chevronUp, chevronDown] = clickParentTarget.children;
+    chevronUp.classList.toggle("hidden");
+    chevronDown.classList.toggle("hidden");
+    questionParagraph.classList.toggle("hidden");
   }
-}
-
-@media (max-width: 59em) {
-  .no-flexbox-gap .main-nav-list li:not(:last-child) {
-    margin-right: 0;
-    margin-bottom: 4.8rem;
-  }
-}
-*/
+});
